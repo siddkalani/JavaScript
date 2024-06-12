023941188dd993b2dee7443d7c9b367eff6dd5d8
siddkalani <siddharthkalani6@gmail.com>
2024-06-12 14:03:40 +0530

    first

---
+++ traversing html using dom/practice.js
@@ -0,0 +1,19 @@
+const createTable = () =>{
+    // creating table
+   const tbl = document.createElement("table")
+
+   for(i=0;i<2;i++){
+    const tblHead = document.createElement("th")
+
+    for(j=0;j<2;j++){
+        const row = document.createElement("tr")
+        const text = document.createTextNode(`this is row ${i}`)
+        row.appendChild(text)
+        tblHead.appendChild(row)
+       }
+       tbl.appendChild(tblHead)
+   }
+   document.body.appendChild(tbl)
+   tbl.setAttribute("border","2");
+   
+}
